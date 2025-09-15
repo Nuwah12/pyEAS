0754606267c536fa8e5954f1c622c5d24fb6be1b
Nuwah12 <nburget1@hotmail.com>
2025-09-14 23:12:13 -0400

    get active alerts; all params implemented

--- pyEAS/api/nws.py
+++ pyEAS/api/nws.py
@@ -31,10 +31,22 @@ def _request(params: Dict, URL: str = URL, timeout: int = 15) -> Dict:
     except ValueError:
         print("[ERROR] Response content is not valid JSON, aborting")
 
+def _filter_response(response: Dict, filter_params: Dict) -> Dict:
+    return {k: v for k, v in response.items()
+            if k not in filter_params or v in filter_params[k]}
+
 def get_active_alerts(
     area: str = "PA", # Default to PA
     status: str = "actual",
     point: Optional[tuple[str, str]] = None,
+    message_type: Optional[str] = None,
+    event: Optional[str] = None,
+    code: Optional[str] = None,
+    region_type: Optional[str] = None,
+    zone: Optional[str] = None,
+    urgency: Optional[str] = None,
+    severity: Optional[str] = None,
+    certainty: Optional[str] = None,
     **kwargs # all others
 ) -> Dict:
     """
@@ -48,32 +60,33 @@ def get_active_alerts(
     """
 
     ### TODO: Handle two different types of arguments that will be trated the same by the user:
-    ###         1. Arguments passed via URL
-    ###         2. Arguments that require filterting of response
+    ###         1. Arguments passed via URL (will be function args)
+    ###         2. Arguments that require filterting of response (kwargs)
     ### Obviously these have to be trated differently, but it'd be good for the user to be able to treat them  the same 
     valid_keys = Settings.NWS.VALID_SEARCH_KEYS
-    params_use = {}
+    url_params = {"area": area, "status": status}
+    filter_params = {}
+    # Check for positional args, they are typed so we can just append them to a list of they were passed (i.e. not None)
+    for var in ["point", "message_type", "event", "code", 
+                "region_type", "zone", "urgency", "severity", "certainty"]:
+        value = locals()[var]
+        if value:
+            url_params[var] = value
+    # Now check kwargs, must verify name and type
     for i in kwargs:
-        if i not in valid_keys:         # Checking param name
-            print(f"[WARN] Passed parameter {i} is not a valid parameter name. Skipping.")
+        if i not in valid_keys.keys():
+            print(f"[WARN] Keyword argument {i} not valid; continuing.")
             continue
-        val = kwargs[i]     # Passed (param, val); we can ensure param name is correct
-        corr_type = valid_keys[i]   # The type val should be
-        if not isinstance(val, corr_type):
-            orig_type = type(val)
-            try:
-                val = corr_type(val)
-            except Exception as e:
-                print(f"[WARN] Could not convert parameter {i} from type {orig_type} to {corr_type}. Leaving as default.")
-                continue
-        params_use[i] = val
 
-    params_use["area"] = area
-    params_use["status"] = status
-    if point:
-        params_use["point"] = point
+        if not isinstance(kwargs[i], valid_keys[i]):
+            print(f"[WARN] type of keyword argument {i} passed was {type(kwargs[i])}, but requires type {valid_keys[i]}")
+            continue
+        
+        filter_params[i] = kwargs[i]
+        
+    resp = _request(url_params).get("features")[0]
 
-    return _request(params_use)
+    return _filter_response(resp, filter_params)
 
 def get_active_alert_count() -> Dict:
     return _request(params = {}, 
